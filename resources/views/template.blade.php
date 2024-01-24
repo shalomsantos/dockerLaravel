945f9d3378f187bc939785672a671085ea8dca74
shalomsantos <shalomsantos1234@gmail.com>
2024-01-24 14:00:43 -0300

    trocando imagem do readme

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -48,34 +48,6 @@
             </a>
         </div>
     </nav>
-    {{-- <nav class="navbar navbar-expand-sm bg-body-tertiary fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/')}}">Shalom Santos</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ url('/')}}">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Tabelas
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ url('/users')}}">Usuários</a></li>
-                            <li><a class="dropdown-item" href="{{ url('/products')}}">Produtos</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/about') }}">Sobre</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav> --}}
     
     <main>
         <section class="position-relative">
